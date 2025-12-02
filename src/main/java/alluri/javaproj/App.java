8bbc8b3317fbcc034817df61d414c4ddfdc7d22e
kc09x7 <krishnachaitanyach.kmit@gmail.com>
2025-12-02 18:32:37 +0530

    a

---
+++ src/main/java/alluri/javaproj/App.java
@@ -0,0 +1,13 @@
+package alluri.javaproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "jai mahishmathi" );
+    }
+}
